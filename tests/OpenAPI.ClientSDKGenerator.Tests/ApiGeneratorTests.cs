dc442f691897adc75dc3315d837d774a5f670730
Fredrik Arvidsson <fresa@fresa.se>
2026-04-24 20:07:20 +0200

    test: refactor setup generator scaffolding

--- tests/OpenAPI.ClientSDKGenerator.Tests/ApiGeneratorTests.cs
+++ tests/OpenAPI.ClientSDKGenerator.Tests/ApiGeneratorTests.cs
@@ -1,16 +1,12 @@
-using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using AwesomeAssertions;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using OpenAPI.ClientSDKGenerator.Tests.Utils;
 using Xunit;
 
 namespace OpenAPI.ClientSDKGenerator.Tests;
 
-public partial class ApiGeneratorTests
+public class ApiGeneratorTests
 {
     private CancellationToken Cancellation => TestContext.Current.CancellationToken;
     
@@ -22,7 +18,7 @@ public partial class ApiGeneratorTests
     [InlineData("openapi-v3.2.yaml")]
     public void GivenAnOpenAPISpec_WhenGeneratingAPI_ExpectedClassesShouldHaveBeenGenerated(string openApiSpec)
     {
-        var compilation = SetupGenerator(openApiSpec,
+        var compilation = Generator.Setup(openApiSpec, Cancellation,
             out var diagnostics);
 
         diagnostics.Should().BeEmpty();
@@ -32,34 +28,5 @@ public partial class ApiGeneratorTests
             .ToArray();
 
         generatedFiles.Should().HaveCount(0);
-    }
-
-    private Compilation SetupGenerator(string openApiSpec, out ImmutableArray<Diagnostic> diagnostics)
-    {
-        var generator = new ApiGenerator();
-
-        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
-
-        driver = driver.AddAdditionalTexts(
-            [
-                new TestAdditionalFile($"OpenApiSpecs/{openApiSpec}")
-            ]
-        );
-
-        const string assemblyName = nameof(ApiGeneratorTests);
-        var compilation = CSharpCompilation.Create(assemblyName,
-            options: new CSharpCompilationOptions(outputKind: OutputKind.DynamicallyLinkedLibrary));
-
-        driver.RunGeneratorsAndUpdateCompilation(compilation, out var newCompilation, out diagnostics,
-            Cancellation);
-        
-        foreach (var tree in newCompilation.SyntaxTrees)                                                  
-        {                                
-            tree.GetDiagnostics().Should().NotContain(diagnostic =>
-                diagnostic.Severity == DiagnosticSeverity.Error ||
-                diagnostic.Severity == DiagnosticSeverity.Warning);       
-        }     
-        
-        return newCompilation;
     }
 }
---
+++ tests/OpenAPI.ClientSDKGenerator.Tests/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using System.Threading;
+using AwesomeAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using OpenAPI.ClientSDKGenerator.Tests.Utils;
+
+namespace OpenAPI.ClientSDKGenerator.Tests;
+
+internal static class Generator
+{
+    internal static Compilation Setup(string openApiSpec, CancellationToken cancellationToken, out ImmutableArray<Diagnostic> diagnostics)
+    {
+        var generator = new ApiGenerator();
+
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+        driver = driver.AddAdditionalTexts(
+            [
+                new TestAdditionalFile($"OpenApiSpecs/{openApiSpec}")
+            ]
+        );
+
+        const string assemblyName = nameof(ApiGeneratorTests);
+        var compilation = CSharpCompilation.Create(assemblyName,
+            options: new CSharpCompilationOptions(outputKind: OutputKind.DynamicallyLinkedLibrary));
+
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var newCompilation, out diagnostics, cancellationToken);
+        
+        foreach (var tree in newCompilation.SyntaxTrees)                                                  
+        {                                
+            tree.GetDiagnostics().Should().NotContain(diagnostic =>
+                diagnostic.Severity == DiagnosticSeverity.Error ||
+                diagnostic.Severity == DiagnosticSeverity.Warning);       
+        }     
+        
+        return newCompilation;
+    }
+}
