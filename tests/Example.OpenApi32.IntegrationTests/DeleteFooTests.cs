bfb6cac722fc91ab7b1c99e873c1884267fbf6c8
Fredrik Arvidsson <fresa@fresa.se>
2026-05-28 19:49:22 +0200

    test: response

--- tests/Example.OpenApi32.IntegrationTests/DeleteFooTests.cs
+++ tests/Example.OpenApi32.IntegrationTests/DeleteFooTests.cs
@@ -1,3 +1,5 @@
+using AwesomeAssertions;
+
 namespace Example.OpenApi32.IntegrationTests;
 
 public class DeleteFooTests(FooApplicationFactory app) : FooTestSpecification, IClassFixture<FooApplicationFactory>
@@ -7,7 +9,8 @@ public class DeleteFooTests(FooApplicationFactory app) : FooTestSpecification, I
     {
         using var httpClient = app.CreateClient();
         var client = new Foo.Foo(httpClient);
-        await client.Foo_(10)
+        var response = await client.Foo_(10)
             .DeleteAsync(CancellationToken);
+        response.Should().BeOfType<Foo.Foo.Foo1.DeleteResponse.OK200.Empty>();
     }
 }
