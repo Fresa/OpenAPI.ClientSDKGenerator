9793affd03a6b29a70300636f31d2ac9a436e88f
Fredrik Arvidsson <fresa@fresa.se>
2026-06-03 00:01:04 +0200

    test(openapi32): add scenarios for the rest of the api

--- tests/Example.OpenApi32.IntegrationTests/UpdateFooTests.cs
+++ tests/Example.OpenApi32.IntegrationTests/UpdateFooTests.cs
@@ -36,31 +36,6 @@ public class UpdateFooTests(FooApplicationFactory app) : FooTestSpecification, I
         // result.Content.Headers.ContentType.Should().Be(MediaTypeHeaderValue.Parse("application/json"));
     }
 
-    // [Fact]
-    // public async Task Given_invalid_request_When_Updating_Foo_It_Should_Return_400()
-    // {
-    //     using var httpClient = app.CreateClient()
-    //         .WithOAuth2ImplicitFlowAuthentication("update");
-    //     var client = CreateClient(httpClient);
-    //
-    //     var responseHandler = new NativeResponseHandler();
-    //     await client.Foo["test"].PutAsync(
-    //         new FooProperties { Name = "test" },
-    //         config =>
-    //         {
-    //             config.Headers.Add("Bar", "test");
-    //             config.Options.Add(new ResponseHandlerOption { ResponseHandler = responseHandler });
-    //         },
-    //         CancellationToken);
-    //     var result = (HttpResponseMessage)responseHandler.Value;
-    //
-    //     result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    //     var responseContent = await result.Content.ReadAsJsonNodeAsync(CancellationToken);
-    //     responseContent.Should().NotBeNull();
-    //     responseContent.AsArray().Should().HaveCount(1);
-    //     responseContent.GetValue<string>("#/0/error").Should().NotBeNullOrEmpty();
-    //     responseContent.GetValue<string>("#/0/name").Should().Be("https://localhost/api.json#/components/parameters/FooId/schema/type");
-    // }
     //
     // [Fact]
     // public async Task Given_unauthenticated_request_When_Updating_Foo_It_Should_Return_401()
